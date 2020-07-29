f54dd05389791e871af52bf84391241e626e9bff
Worasit Daimongkol <worasit.dmk501@gmail.com>
2020-07-29 09:13:34 +0700

    Added hibernate examples.

---
+++ src/main/java/com/daimongkol/aws/jpa/EmployeeEntity.java
@@ -0,0 +1,71 @@
+package com.daimongkol.aws.jpa;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "employee", schema = "public", catalog = "agoda_core")
+public class EmployeeEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Integer salary;
+
+    @Id
+    @Column(name = "id", updatable = false, unique = true)
+    @GeneratedValue(generator = "employee_generator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "employee_generator", sequenceName = "employee_id_seq", catalog = "agoda_core", schema = "public", allocationSize = 0)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "salary")
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeEntity that = (EmployeeEntity) o;
+        return id == that.id &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(salary, that.salary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, salary);
+    }
+}
---
+++ src/main/java/com/daimongkol/aws/jpa/JpaBoot.java
@@ -0,0 +1,39 @@
+package com.daimongkol.aws.jpa;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class JpaBoot {
+    public static void main(String[] args) {
+
+        // Create registry
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+
+        // Create MetadataSources
+        MetadataSources sources = new MetadataSources(registry);
+
+        // Create Metadata
+        Metadata metadata = sources.getMetadataBuilder().build();
+
+        // Create SessionFactory
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            EmployeeEntity employeeEntity = new EmployeeEntity();
+            employeeEntity.setFirstName("worasit");
+            employeeEntity.setLastName("daimongkol");
+            employeeEntity.setSalary(120000);
+
+            session.save(employeeEntity);
+            transaction.commit();
+            sessionFactory.close();
+        }
+    }
+}
